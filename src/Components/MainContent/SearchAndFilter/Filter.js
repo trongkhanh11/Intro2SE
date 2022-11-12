aa70706b99a10a2b641b5434e84a6cf9f2cf7c3e
Khanh <truongtrongkhanhcm@gmail.com>
2022-11-12 15:58:20 +0700

    Add Filter

--- src/Components/MainContent/SearchAndFilter/Filter.js
+++ src/Components/MainContent/SearchAndFilter/Filter.js
@@ -1,12 +1,105 @@
-import React from "react";
+import React, { useContext } from "react";
+import {GiEarthAsiaOceania, GiWorld} from 'react-icons/gi';
+import {FaChevronDown, FaGlobeAfrica, FaGlobeAmericas, FaGlobeAsia, FaGlobeEurope} from 'react-icons/fa';
+import styled from "styled-components";
+import {ThemeContext} from '../../ThemeContext/themeContext';
 
 function Filter(props) {
+    const themeContext = useContext(ThemeContext);
     return (
-        <div>
+        <FilterPane>
             <h3>Filter by regions:</h3>
-            <span>All</span>
-        </div>
+            <SelectPane>
+                <Select className={themeContext.theme}>
+                    <span>All</span>
+                    <FaChevronDown />
+                </Select>
+                <OptionsPane className={themeContext.theme}>
+                    <OptionItem>
+                        <GiWorld />
+                        <span>All</span>
+                    </OptionItem>
+                    <OptionItem>
+                        <FaGlobeAmericas />
+                        <span>Americas</span>
+                    </OptionItem>
+                    <OptionItem>
+                        <FaGlobeAsia />
+                        <span>Asia</span>
+                    </OptionItem>
+                    <OptionItem>
+                        <FaGlobeEurope />
+                        <span>Europe</span>
+                    </OptionItem>
+                    <OptionItem>
+                        <GiEarthAsiaOceania />
+                        <span>Oceania</span>
+                    </OptionItem>
+                </OptionsPane>
+            </SelectPane>
+        </FilterPane>
     )
 }
 
 export default Filter;
+
+const FilterPane = styled.div`
+    max-width: 160px;
+    width: 100%;
+    margin-top: 20px;
+
+    h3 {
+        font-size: 18px;
+        font-weight: 600;
+        text-shadow: var(--TextShadow);
+    }
+`;
+
+const SelectPane = styled.div`
+    width: 100%;
+    margin-top: 8px;
+    position: relative;
+    background: #ccc;
+`;
+
+const Select = styled.div`
+    padding: 0 8px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 4px;
+    height: 34px;
+    user-select: none;
+
+    span {
+        font-size: 18px;
+        font-weight: bold;
+    }
+`;
+
+const OptionsPane = styled.ul`
+    width: 100%;
+    margin-top: 2px;
+    border-radius: 4px;
+    position: absolute;
+    overflow: hidden;
+    z-index: 10;
+`;
+
+const OptionItem = styled.li`
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 500;
+    cursor: pointer;
+    padding: 4px 8px;
+
+    &:hover {
+        font-weight: bold;
+        background: var(--SelectOptionBackground);
+    }
+
+    span {
+        margin-left: 4px;
+    }
+`;
